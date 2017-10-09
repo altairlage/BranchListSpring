01fa6e8d9528c0ee3cf438eedd04639c0fe6896d
BRTOKEN\Altair <altair.lage@datablink.com>
2017-10-09 12:43:24 -0300

    Project init

---
+++ BranchListSpring/src/main/java/com/brachlist/config/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,9 @@
+package com.brachlist.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
+
+	
+	
+}
